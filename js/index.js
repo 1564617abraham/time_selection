de4917e8b438825698707f6e8004a7bb85dfd7e8
Abraham <1564617@uab.cat>
2022-09-14 21:55:49 +0200

    primeras pruebas

---
+++ js/index.js
@@ -0,0 +1,16 @@
+class Persona {
+    "nombre"
+    "numero"
+    "turnos"
+    constructor(nombre, numero, turnos) {
+        this.nombre = nombre,
+        this.numero = numero,
+        this.turnos = turnos
+    }
+}
+
+let User = new Persona("Juan", 56, ["cube", "rampa"])
+
+document.getElementById("nombre").innerHTML = User.nombre
+document.getElementById("numero").innerHTML = User.numero
+document.getElementById("turno").innerHTML = User.turnos
